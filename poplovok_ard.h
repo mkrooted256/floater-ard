fa068599aa44fae28131e121de91d36b68bbb4e1
mkrooted256 <koreshov.m@gmail.com>
2024-03-24 08:01:27 +0200

    remove everything, leave essentials only

--- poplovok_ard.h
+++ poplovok_ard.h
@@ -3,7 +3,7 @@
 
 #include "Arduino.h"
 
-#define POPLOVOK_VER 1 
+#define POPLOVOK_VER 2
 
 // Status LED, output
 #define PIN_STATUSLED 5
@@ -11,15 +11,15 @@
 #define PIN_LOWER_SENSOR 2
 // Upper sensor, input_pullup, default HIGH, impulse LOW
 #define PIN_UPPER_SENSOR 3
+// Relay
+#define PIN_PUMP 6
+#define PUMP_ON HIGH
+#define PUMP_OFF LOW
 
 // Note:  interrupt pins for Nano are 	2, 3
 
-// time to wait for serial input before entering production mode, in ms
-#define SERIAL_INIT_TIME 3000
 // time to ignore subsequent sensor impulses, in ms
 #define SENSOR_DEBOUNCING_TIME 500
-// check if sensors are connected if no impulse for this period, in ms
-#define SENSORS_CHECK_INTERVAL 30000
 
 // Device state
 struct SensorUpdateTime {
@@ -43,141 +43,3 @@ void log(Log::Level level, String msg) {
     Serial.println(line);
 }
 
-// Signaling
-namespace Signaling {
-    enum Signal {
-        O_INIT=0,
-        O_ST_NOT_INITIALIZED,
-        O_ST_IDLING,
-        O_ST_PUMPING,
-        O_ERROR
-    };
-    
-    struct BlinkSequence {
-        const unsigned long beatDuration; // ms
-        const unsigned int len; // len of sequence
-        const char* sequence; // '1'/'0' sequence
-    };
-
-    const BlinkSequence seq_init{1000, 1, "1"};
-    const BlinkSequence seq_not_initialized{125, 10, "1010100000"};
-    const BlinkSequence seq_idling{125, 8, "10000000"};
-    const BlinkSequence seq_pumping{125, 8, "11111100"};
-    const BlinkSequence seq_error{500, 2, "10"};
-
-    struct Blinker {
-        const BlinkSequence* currentSequence = nullptr;
-        unsigned int beatNumber = 0;
-        unsigned long lastUpdateTime = 0;
-
-        void play(const BlinkSequence* seq) {
-            currentSequence = seq;
-            beatNumber = 0;
-            lastUpdateTime = 0;
-            bool nextLevel = HIGH ? (currentSequence->sequence[0] == '1') : LOW;
-            digitalWrite(PIN_STATUSLED, nextLevel);
-            log(Log::VERBOSE, F("blinker::play"));
-        }
-
-        void stop() {
-            digitalWrite(PIN_STATUSLED, LOW);
-            currentSequence = nullptr;
-            log(Log::VERBOSE, F("blinker::stop"));
-        }
-
-        void advanceBeat() {
-            beatNumber = (beatNumber + 1) % currentSequence->len;
-            bool nextLevel = HIGH ? (currentSequence->sequence[beatNumber] == '1') : LOW;
-            digitalWrite(PIN_STATUSLED, nextLevel);
-        }
-
-        void loop() {
-            if (!currentSequence) return;
-            if (millis() - lastUpdateTime >= currentSequence->beatDuration) {
-                advanceBeat();
-                lastUpdateTime = millis();
-            }
-        }
-    };
-};
-
-//
-namespace NormalMode {
-    enum State {
-        // No impulses yet
-        ST_NOT_INITIALIZED = 0,
-        ST_IDLING,
-        ST_PUMPING,
-    };
-    enum Sensor {
-        SEN_UPPER = 1,
-        SEN_LOWER,
-    };
-
-    struct Mode {
-        State state = ST_NOT_INITIALIZED;
-        Signaling::Blinker blinker;
-        SensorUpdateTime handledImpulseTimes{0,0};
-        volatile SensorUpdateTime lastImpulseTimes{0,0};
-
-        // blocking!
-        // inputs should be HIGH for ~500ms straight.
-        bool verifySensorsConnected() {
-            for (int i=0; i<50; i++) {
-                delay(10);
-                if (digitalRead(PIN_LOWER_SENSOR) == LOW) return false;
-                if (digitalRead(PIN_UPPER_SENSOR) == LOW) return false;
-            }
-            return true;
-        }
-
-        void initialize() {
-            if (state == ST_NOT_INITIALIZED) {
-                if (!verifySensorsConnected()) {
-                    blinker.play(&Signaling::seq_error);
-                    log(Log::WARNING, F("Failed to initialize NormalMode - sensors are disconnected. ST_NOT_INITIALIZED."));
-                    return;
-                }
-
-                state = ST_IDLING;
-                blinker.play(&Signaling::seq_idling);
-                log(Log::LOG, F("Initializing NormalMode. ST_IDLING."));
-            }
-        }
-
-        void triggerLowerSensor() {
-            if (state == ST_PUMPING) {
-                state = ST_IDLING;
-                blinker.play(&Signaling::seq_idling);
-                log(Log::LOG, F("triggerLowerSensor - to ST_IDLING"));
-            }
-        }
-
-        void triggerUpperSensor() {
-            if (state == ST_IDLING) {
-                state = ST_PUMPING;
-                blinker.play(&Signaling::seq_pumping);
-                log(Log::LOG, F("triggerUpperSensor - to ST_PUMPING"));
-            }
-        }
-
-        void loop() {
-            // To maintain const value throughout whole function (lastImpulseTimes is volatile)
-            SensorUpdateTime cur_lastImpulseTimes{lastImpulseTimes.lower_sensor, lastImpulseTimes.upper_sensor}; 
-            bool impulseDetected = false;
-
-            if (cur_lastImpulseTimes.lower_sensor - handledImpulseTimes.lower_sensor > SENSOR_DEBOUNCING_TIME) {
-                triggerLowerSensor();
-                handledImpulseTimes.lower_sensor = cur_lastImpulseTimes.lower_sensor;
-                impulseDetected = true;
-            }
-            if (cur_lastImpulseTimes.upper_sensor - handledImpulseTimes.upper_sensor > SENSOR_DEBOUNCING_TIME) {
-                triggerUpperSensor();
-                handledImpulseTimes.upper_sensor = cur_lastImpulseTimes.upper_sensor;
-                impulseDetected = true;
-            }
-
-            blinker.loop();
-        }
-    };
-};
--- poplovok_ard.ino
+++ poplovok_ard.ino
@@ -2,7 +2,17 @@
 
 
 // State
-NormalMode::Mode mode;
+enum State {
+    ST_SETUP,
+    ST_PUMPING,
+    ST_IDLING
+};
+
+State globalState = ST_SETUP;
+SensorUpdateTime handledImpulseTime{0,0};
+
+// Changed in interrupts
+volatile SensorUpdateTime lastImpulseTime{0,0};
 
 // Initialization
 void setup_inputs() {
@@ -10,32 +20,71 @@ void setup_inputs() {
     pinMode(PIN_STATUSLED, OUTPUT);
     pinMode(PIN_LOWER_SENSOR, INPUT_PULLUP);
     pinMode(PIN_UPPER_SENSOR, INPUT_PULLUP);
+    pinMode(PIN_PUMP, OUTPUT);
+
+    digitalWrite(PIN_STATUSLED, LOW);
+    digitalWrite(PIN_PUMP, PUMP_OFF);
 
     Serial.begin(9600);
 }
 
 // Interrupts
 void lowerSensorISR() {
-    mode.lastImpulseTimes.lower_sensor = millis();
+    lastImpulseTime.lower_sensor = millis();
 }
 void upperSensorISR() {
-    mode.lastImpulseTimes.upper_sensor = millis();
+    lastImpulseTime.upper_sensor = millis();
 }
 
 // Main
 void setup() {
-    init();
+    setup_inputs();
     
-    log(Log::LOG, F("Setup"));
-    mode.initialize();
+    log(Log::LOG, F("Hello"));
 
     log(Log::LOG, F("Setting up interrupts"));
+    // FALLING because inputs configured as input_pullup, default state is HIGH, and impulse is LOW.
     attachInterrupt(digitalPinToInterrupt(PIN_LOWER_SENSOR), lowerSensorISR, FALLING);
     attachInterrupt(digitalPinToInterrupt(PIN_UPPER_SENSOR), upperSensorISR, FALLING);
     
     log(Log::LOG, F("Ready"));
+    globalState = ST_IDLING;
+}
+
+void applyState(State newState) {
+    if (globalState == ST_PUMPING && newState == ST_IDLING) {
+        log(Log::LOG, F("ST_PUMPING -> ST_IDLING"));
+        log(Log::LOG, F("stop pump"));
+        digitalWrite(PIN_PUMP, PUMP_OFF);
+        digitalWrite(PIN_STATUSLED, LOW);
+    }
+    if (globalState == ST_IDLING && newState == ST_PUMPING) {
+        log(Log::LOG, F("ST_IDLING -> ST_PUMPING"));
+        log(Log::LOG, F("run pump"));
+        digitalWrite(PIN_PUMP, PUMP_ON);
+        digitalWrite(PIN_STATUSLED, HIGH);
+    }
+
+    globalState = newState;
 }
 
 void loop() {
-    mode.loop();
+    if (globalState == ST_IDLING) 
+    {
+        if (lastImpulseTime.upper_sensor != handledImpulseTime.upper_sensor) 
+        {
+            applyState(ST_PUMPING);
+            handledImpulseTime.upper_sensor = lastImpulseTime.upper_sensor;
+        }
+    } 
+    else
+    if (globalState == ST_PUMPING) 
+    {
+        if (lastImpulseTime.lower_sensor != handledImpulseTime.lower_sensor) 
+        {
+            applyState(ST_IDLING);
+            handledImpulseTime.lower_sensor = lastImpulseTime.lower_sensor;
+        }
+    }
+
 }
